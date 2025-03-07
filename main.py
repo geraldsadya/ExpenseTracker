c7feb74b2a36318db43ad562170acac0136a34d3
Gerald Sadya <sadyageraldm@gmail.com>
2025-03-07 14:58:08 +0200

    Added command to add new expenses as well as the function to add an expense according to fields needed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 import os
+from datetime import datetime
 
 
 def create_parser():
@@ -11,8 +12,10 @@ def create_parser():
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
     "The help argument describes what these sub-commands are for."
 
-    #test:hello (just to test the CLI)
-    hello_parser = subparsers.add_parser("hello", help="Say hello")
+    #command to add new expense
+    add_parser = subparsers.add_parser("add", help="Add a new expense")
+    add_parser.add_argument("--description", required=True, help="Description of the expense")
+    add_parser.add_argument("--amount", type=float, required=True, help="Amount of the expense")
     return parser
 
 def load_data(file_path="expenses.json"):
@@ -30,6 +33,24 @@ def save_data(data, file_path="expenses.json"):
     with open(file_path, "w") as file:
         json.dump(data, file, indent=4)
 
+
+def add_expense(description, amount, file_path="expenses.json"):
+    expenses = load_data(file_path)#Load existing expenses
+    new_id = expenses[-1]["id"] + 1 if expenses else 1
+
+    #Creating the new expense entry with the current date so i have to import data and time
+    new_expense = {
+        "id": new_id,
+        "date": datetime.now().strftime("%Y-%m-%d"),
+        "description": description,
+        "amount": amount
+    }
+
+    #Append and save the data
+    expenses.append(new_expense)
+    save_data(expenses, file_path)
+    print(f"Expense added successfully (ID: {new_id})")
+
 def main():
     parser = create_parser()#Create the argument parser
     args = parser.parse_args() #Parse the arguments from the command line
@@ -37,8 +58,9 @@ def main():
     #Check which command was used
     if args.command == "hello":
         print("Hello from Expense Tracker!")
+    elif args.command == "add":
+        add_expense(args.description, args.amount)
     else:
-        #If no command or an unrecognized command is provided, print help
         parser.print_help()
 
 if __name__ == "__main__":
